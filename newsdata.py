42aa5cd03b31e54b15b1f66e78df54345f8bd2b8
Thadius <thadiusp@gmail.com>
2018-12-20 13:37:01 -0500

    Add python code to answer question 2

--- newsdata.py
+++ newsdata.py
@@ -1,10 +1,23 @@
 import psycopg2
 
 #Answers the question of the most popular article
+#def art:
 connection = psycopg2.connect('news')
 cursor = connection.cursor()
 cursor.execute(
     'SELECT articles.title, count(*) AS num FROM articles JOIN log ON log.path LIKE concat("%", articles.log, "%") GROUP BY articles.title ORDER BY num DESC LIMIT 3'
+)
+results = cursor.fetchall()
+print(results)
+connection.close()
+
+
+#Answers the question of the most popular author
+#def auth:
+connection = psycopg2.connect('news')
+cursor = connection.cursor()
+cursor.execute(
+    ''
 )
 results = cursor.fetchall()
 print(results)
