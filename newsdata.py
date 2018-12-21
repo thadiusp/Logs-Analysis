b6427993874ebea517d1e9b51613983ff16b02b3
Thadius <thadiusp@gmail.com>
2018-12-21 15:32:23 -0500

    ADD sql code to answer question 2

--- newsdata.py
+++ newsdata.py
@@ -1,23 +1,23 @@
 import psycopg2
 
-#Answers the question of the most popular article
-#def art:
-connection = psycopg2.connect('news')
+# Answers the question of the most popular article
+# def art:
+connection = psycopg2.connect(database='news')
 cursor = connection.cursor()
 cursor.execute(
-    'SELECT articles.title, count(*) AS num FROM articles JOIN log ON log.path LIKE concat("%", articles.log, "%") GROUP BY articles.title ORDER BY num DESC LIMIT 3'
+    "SELECT articles.title, count(*) AS num FROM articles JOIN log ON log.path LIKE concat('%', articles.slug, '%') GROUP BY articles.title ORDER BY num DESC LIMIT 3"
 )
 results = cursor.fetchall()
 print(results)
 connection.close()
 
 
-#Answers the question of the most popular author
-#def auth:
-connection = psycopg2.connect('news')
+# Answers the question of the most popular author
+# def auth:
+connection = psycopg2.connect(database='news')
 cursor = connection.cursor()
 cursor.execute(
-    ''
+    "SELECT authors.name, count(*) AS num FROM authors, articles, log WHERE authors.id = articles.author AND log.path like concat('%', articles.slug, '%') GROUP BY authors.name ORDER BY num DESC"
 )
 results = cursor.fetchall()
 print(results)
