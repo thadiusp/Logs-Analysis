6af5395261e8bec5c2f0171fd729b9daae6d5c81
Thadius <thadiusp@gmail.com>
2018-12-23 11:45:13 -0500

    Add code to answer question 3

--- newsdata.py
+++ newsdata.py
@@ -22,3 +22,14 @@ cursor.execute(
 results = cursor.fetchall()
 print(results)
 connection.close()
+
+
+# Answers the question of which days had 1% or more errors
+connection = psycopg2.connect(database='news')
+cursor = connection.cursor()
+cursor.execute(
+    "SELECT CAST(time as date) as date, percent FROM (SELECT CAST(time as date), round(count(CASE status WHEN '404 NOT FOUND' THEN 1 ELSE null END) * 100 / sum(count(status)) OVER (PARTITION by CAST(time as date)), 1) as percent FROM log GROUP BY CAST(time as date)) as date WHERE percent >= 1"
+)
+results = cursor.fetchall()
+print(results)
+connection.close()
