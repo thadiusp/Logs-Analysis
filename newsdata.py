98abc5191fe1c82fc26b02be5dc403755425ae54
Thadius <thadiusp@gmail.com>
2018-12-23 14:38:07 -0500

    Add pretty table to better display results

--- newsdata.py
+++ newsdata.py
@@ -1,35 +1,48 @@
 import psycopg2
+from prettytable import from_db_cursor
 
-# Answers the question of the most popular article
-# def art:
+# table = PrettyTable()
+
+# Answers the question of the most popular articles
+print('What are the most popular articles of all time?')
 connection = psycopg2.connect(database='news')
-cursor = connection.cursor()
-cursor.execute(
-    "SELECT articles.title, count(*) AS num FROM articles JOIN log ON log.path LIKE concat('%', articles.slug, '%') GROUP BY articles.title ORDER BY num DESC LIMIT 3"
-)
-results = cursor.fetchall()
-print(results)
+
+with connection:
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT articles.title, count(*) AS num FROM articles JOIN log ON log.path LIKE concat('%', articles.slug, '%') GROUP BY articles.title ORDER BY num DESC LIMIT 3"
+    )
+
+    x = from_db_cursor(cursor)
+print(x)
 connection.close()
 
 
 # Answers the question of the most popular author
-# def auth:
+print('Who are the most popular authors?')
 connection = psycopg2.connect(database='news')
-cursor = connection.cursor()
-cursor.execute(
-    "SELECT authors.name, count(*) AS num FROM authors, articles, log WHERE authors.id = articles.author AND log.path like concat('%', articles.slug, '%') GROUP BY authors.name ORDER BY num DESC"
-)
-results = cursor.fetchall()
-print(results)
+
+with connection:
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT authors.name, count(*) AS num FROM authors, articles, log WHERE authors.id = articles.author AND log.path like concat('%', articles.slug, '%') GROUP BY authors.name ORDER BY num DESC"
+    )
+
+    x = from_db_cursor(cursor)
+print(x)
 connection.close()
 
 
 # Answers the question of which days had 1% or more errors
+print('On which days did more than 1% of requests lead to errors?')
 connection = psycopg2.connect(database='news')
-cursor = connection.cursor()
-cursor.execute(
-    "SELECT CAST(time as date) as date, percent FROM (SELECT CAST(time as date), round(count(CASE status WHEN '404 NOT FOUND' THEN 1 ELSE null END) * 100 / sum(count(status)) OVER (PARTITION by CAST(time as date)), 1) as percent FROM log GROUP BY CAST(time as date)) as date WHERE percent >= 1"
-)
-results = cursor.fetchall()
-print(results)
+
+with connection:
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT CAST(time as date) as date, percent FROM (SELECT CAST(time as date), round(count(CASE status WHEN '404 NOT FOUND' THEN 1 ELSE null END) * 100 / sum(count(status)) OVER (PARTITION by CAST(time as date)), 1) as percent FROM log GROUP BY CAST(time as date)) as date WHERE percent >= 1"
+    )
+
+    x = from_db_cursor(cursor)
+print(x)
 connection.close()
