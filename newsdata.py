b1a6d027722528c73d0cbd1780d396fd3d205c72
Thadius <thadiusp@gmail.com>
2018-12-24 10:46:27 -0500

    Refactor code to PEP8 style

--- newsdata.py
+++ newsdata.py
@@ -11,7 +11,9 @@ connection = psycopg2.connect(database='news')
 with connection:
     cursor = connection.cursor()
     cursor.execute(
-        "SELECT articles.title, count(*) AS num FROM articles JOIN log ON log.path LIKE concat('%', articles.slug, '%') GROUP BY articles.title ORDER BY num DESC LIMIT 3"
+        """SELECT articles.title, count(*) AS num
+        FROM articles JOIN log ON log.path LIKE concat('%', articles.slug, '%')
+        GROUP BY articles.title ORDER BY num DESC LIMIT 3"""
     )
 
     x = from_db_cursor(cursor)
@@ -26,7 +28,11 @@ connection = psycopg2.connect(database='news')
 with connection:
     cursor = connection.cursor()
     cursor.execute(
-        "SELECT authors.name, count(*) AS num FROM authors, articles, log WHERE authors.id = articles.author AND log.path like concat('%', articles.slug, '%') GROUP BY authors.name ORDER BY num DESC"
+        """SELECT authors.name, count(*) AS num
+        FROM authors, articles, log
+        WHERE authors.id = articles.author AND log.path
+        like concat('%', articles.slug, '%')
+        GROUP BY authors.name ORDER BY num DESC"""
     )
 
     x = from_db_cursor(cursor)
@@ -41,7 +47,12 @@ connection = psycopg2.connect(database='news')
 with connection:
     cursor = connection.cursor()
     cursor.execute(
-        "SELECT CAST(time as date) as date, percent FROM (SELECT CAST(time as date), round(count(CASE status WHEN '404 NOT FOUND' THEN 1 ELSE null END) * 100 / sum(count(status)) OVER (PARTITION by CAST(time as date)), 1) as percent FROM log GROUP BY CAST(time as date)) as date WHERE percent >= 1"
+        """SELECT CAST(time as date) as date, percent
+        FROM (SELECT CAST(time as date),
+        round(count(CASE status WHEN '404 NOT FOUND' THEN 1 ELSE null END)
+        * 100 / sum(count(status))
+        OVER (PARTITION by CAST(time as date)), 1) as percent
+        FROM log GROUP BY CAST(time as date)) as date WHERE percent >= 1"""
     )
 
     x = from_db_cursor(cursor)
